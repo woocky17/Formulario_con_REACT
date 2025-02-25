224325f8f8d217668481e64eb60e6b985cfb5f44
Woocky17 <davidortetsabater@gmail.com>
2025-02-25 18:49:53 +0100

    feat: setting up mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <MantineProvider>
     <App />
-  </StrictMode>,
-)
+  </MantineProvider>
+);
