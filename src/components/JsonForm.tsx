0fbbdca37651b053aaf22d50fb763c8f57d6b19d
raulmaqueda2 <[200~pl2023322@365.stucom.com>
2025-03-11 19:00:25 +0100

    feat: add formulario

--- src/components/JsonForm.tsx
+++ src/components/JsonForm.tsx
@@ -1,24 +1,75 @@
-import { Button, Stack, TextInput, Textarea, Select } from "@mantine/core";
+import {
+  Button,
+  Stack,
+  TextInput,
+  Textarea,
+  Select,
+  Radio,
+  Group,
+} from "@mantine/core";
 import questions from "../assets/cuestionarios.json";
+import { TextoAnimado } from "./TextoAnimado";
+import {
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+  useState,
+} from "react";
 
-function inputTextGen(inputElement: any) {
+function inputTextGen(inputElement: any, indice: any) {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState<string | null>(null);
+
   switch (inputElement.tipo) {
     case "text":
+      const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const inputValue = event.target.value;
+        setValue(inputValue);
+        if (inputValue.length > 0 && inputValue.length < inputElement.restricciones.min) {
+          setError("Debe tener al menos "+inputElement.restricciones.min+" caracteres");
+        } else if (inputValue.length > inputElement.restricciones.max) {
+          setError("No puede superar los "+inputElement.restricciones.max+" caracteres");
+        } else {
+          setError(null);
+        }
+      };
       return (
-          <TextInput 
-            id={inputElement.id}
-            label={inputElement.pregunta}
-          />
+        <TextInput
+          id={inputElement.id}
+          label={TextoAnimado(inputElement.pregunta, indice)}
+          onChange={handleChange}
+          error={error}
+        />
       );
     case "select":
       return (
-        <Select 
+        <Select
           id={inputElement.id}
-          label={inputElement.pregunta}
+          label={TextoAnimado(inputElement.pregunta, indice)}
           data={inputElement.opciones}
         />
-      )
-
+      );
+    case "check":
+      return (
+        <Radio.Group
+          name={inputElement.id}
+          label={TextoAnimado(inputElement.pregunta, indice)}
+        >
+          <Group mt="xs">
+            {inputElement.opciones.map((element: any) => {
+              return <Radio value={element} label={element} />;
+            })}
+          </Group>
+        </Radio.Group>
+      );
+    case "textarea":
+      return (
+        <Textarea
+          id={inputElement.id}
+          label={TextoAnimado(inputElement.pregunta, indice)}
+        />
+      );
     default:
       break;
   }
@@ -29,10 +80,18 @@ function inputTextGen(inputElement: any) {
   );
 }
 
-function formGen(form: any) {
-  return form.preguntas.map((element: any) => {
-    return inputTextGen(element);
-  });
+function formGen(form: any, key: any) {
+  let indice = 0;
+  return (
+    <form key={key}>
+      <h1>{form.titulo}</h1>
+      {form.preguntas.map((element: any) => {
+        let indiceTemp = indice;
+        indice = indice + element.pregunta.length;
+        return <>{inputTextGen(element, indiceTemp)}</>;
+      })}
+    </form>
+  );
 }
 
 const JsonForm = () => {
