eb369fe22a20688c8d61f3bf58a963f537ecae10
Woocky17 <davidortetsabater@gmail.com>
2025-03-13 18:31:38 +0100

    refactor: forms

--- src/components/JsonForm.tsx
+++ src/components/JsonForm.tsx
@@ -1,36 +1,34 @@
-<<<<<<< HEAD
-import {
-  Button,
-  Stack,
-  TextInput,
-  Textarea,
-  Select,
-  Radio,
-  Group,
-} from "@mantine/core";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { TextInput, Textarea, Select, Radio, Group } from "@mantine/core";
 import questions from "../assets/cuestionarios.json";
 import { TextoAnimado } from "./TextoAnimado";
-import {
-  ReactElement,
-  JSXElementConstructor,
-  ReactNode,
-  ReactPortal,
-  useState,
-} from "react";
+import { useState } from "react";
 
-function inputTextGen(inputElement: any, indice: any) {
+function InputTextGen(inputElement: any, indice: any) {
   const [value, setValue] = useState("");
   const [error, setError] = useState<string | null>(null);
+  console.log(value);
 
   switch (inputElement.tipo) {
-    case "text":
+    case "text": {
       const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const inputValue = event.target.value;
         setValue(inputValue);
-        if (inputValue.length > 0 && inputValue.length < inputElement.restricciones.min) {
-          setError("Debe tener al menos "+inputElement.restricciones.min+" caracteres");
+        if (
+          inputValue.length > 0 &&
+          inputValue.length < inputElement.restricciones.min
+        ) {
+          setError(
+            "Debe tener al menos " +
+              inputElement.restricciones.min +
+              " caracteres"
+          );
         } else if (inputValue.length > inputElement.restricciones.max) {
-          setError("No puede superar los "+inputElement.restricciones.max+" caracteres");
+          setError(
+            "No puede superar los " +
+              inputElement.restricciones.max +
+              " caracteres"
+          );
         } else {
           setError(null);
         }
@@ -43,6 +41,7 @@ function inputTextGen(inputElement: any, indice: any) {
           error={error}
         />
       );
+    }
     case "select":
       return (
         <Select
@@ -87,9 +86,9 @@ function formGen(form: any, key: any) {
     <form key={key}>
       <h1>{form.titulo}</h1>
       {form.preguntas.map((element: any) => {
-        let indiceTemp = indice;
+        const indiceTemp = indice;
         indice = indice + element.pregunta.length;
-        return <>{inputTextGen(element, indiceTemp)}</>;
+        return <>{InputTextGen(element, indiceTemp)}</>;
       })}
     </form>
   );
@@ -102,50 +101,58 @@ const JsonForm = () => {
         return formGen(element, key);
       })}
     </>
-=======
-import { Button, Container, TextInput } from "@mantine/core";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-const JsonForm = () => {
-  const navigate = useNavigate();
-  const [form, setForm] = useState(0);
-  const [formData, setFormData] = useState<string[]>([]);
-
-  useEffect(() => {
-    localStorage.setItem("formData", JSON.stringify(formData));
-    if (form === 4) {
-      navigate("/resume");
-    }
-  }, [form, navigate, formData]);
-
-  const nextForm = () => {
-    setForm((current) => current + 1);
-  };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newFormData = [...formData];
-    newFormData[form] = e.currentTarget.value;
-    setFormData(newFormData);
-  };
-
-  return (
-    <Container size="md" style={{ textAlign: "center", padding: "50px 0" }}>
-      <h1>Form {form}</h1>
-      <TextInput
-        value={formData[form] || ""}
-        onChange={handleInputChange}
-      ></TextInput>
-      <Button
-        onClick={() => {
-          nextForm();
-        }}
-      >
-        Next
-      </Button>
-    </Container>
->>>>>>> 5f79f347f00462d56230abe5bf48eb4f8875c4bc
   );
 };
 
 export default JsonForm;
+
+// const JsonForm = () => {
+//   return (
+//     <>
+//       {questions.map((element, key) => {
+//         return formGen(element, key);
+//       })}
+//     </>
+// import { Button, Container, TextInput } from "@mantine/core";
+// import { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// const JsonForm = () => {
+//   const navigate = useNavigate();
+//   const [form, setForm] = useState(0);
+//   const [formData, setFormData] = useState<string[]>([]);
+
+//   useEffect(() => {
+//     localStorage.setItem("formData", JSON.stringify(formData));
+//     if (form === 4) {
+//       navigate("/resume");
+//     }
+//   }, [form, navigate, formData]);
+
+//   const nextForm = () => {
+//     setForm((current) => current + 1);
+//   };
+
+//   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const newFormData = [...formData];
+//     newFormData[form] = e.currentTarget.value;
+//     setFormData(newFormData);
+//   };
+
+//   return (
+//     <Container size="md" style={{ textAlign: "center", padding: "50px 0" }}>
+//       <h1>Form {form}</h1>
+//       <TextInput
+//         value={formData[form] || ""}
+//         onChange={handleInputChange}
+//       ></TextInput>
+//       <Button
+//         onClick={() => {
+//           nextForm();
+//         }}
+//       >
+//         Next
+//       </Button>
+//     </Container>
+//   );
+// };
