70a2c0555593d8e8c7d374f8b3bc3eb7c11cfeb1
judith <judith.bello.torralbo@gmail.com>
2025-03-13 18:26:30 +0100

    Guardados datos en localStorage y validación primer form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WelcomePage from './components/WelcomePage';
@@ -7,6 +7,19 @@ import AcademicForm from './components/AcademicForm';
 
 function App() {
   const [formularioActual, setFormularioActual] = useState(0);
+  const [respuestas, setRespuestas] = useState(() => {
+    const respuestasGuardadas = localStorage.getItem('respuestas');
+    return respuestasGuardadas ? JSON.parse(respuestasGuardadas): {};
+  });
+
+
+  // Al clicar el botón 'Reset' se eliminan las respuestas guardadas y vuelve al inicio
+  // Esta función se pasa como prop a los formularios
+  const handleReset = () => {
+    setRespuestas({});
+    setFormularioActual(0);
+    localStorage.removeItem('respuestas');
+  }
 
   //<> para devolver varios elementos sin tener que envolverlos en un div
   return (
@@ -30,10 +43,23 @@ function App() {
         }
 
         {/** FORMULARIO PERSONAL */}
-        {formularioActual === 1 && <PersonalForm nextForm={() => setFormularioActual(2)}/>}
+        {formularioActual === 1 && 
+        <>
+          <PersonalForm nextForm={() => setFormularioActual(2)}
+          resetForm={handleReset}
+          respuestasGuardadas={respuestas}
+          />
+        </>
+        }
+        
       
         {/** FORMULARIO ACADÉMICO */}
-        {formularioActual === 2 && <AcademicForm nextForm={() => setFormularioActual(3)}/>}
+        {formularioActual === 2 &&
+        <>
+          <AcademicForm nextForm={() => setFormularioActual(3)}/>
+          <button onClick={handleReset} className='btn btn-success'>Reset</button>
+        </>
+        }
       
       </main>
 
--- src/components/PersonalForm.tsx
+++ src/components/PersonalForm.tsx
@@ -28,6 +28,9 @@ interface Cuestionario {
 // Interfaz para definir las propiedades que recibirá el componente
 interface PersonalFormProps {
     nextForm: () => void;
+    resetForm: () => void;
+    respuestasGuardadas?: any;
+    setRespuestas?: React.Dispatch<React.SetStateAction<any>>;  // Recibimos la función setRespuestas desde el padre
 }
 
 /** 
@@ -35,23 +38,41 @@ interface PersonalFormProps {
  * <PersonalFormProps> -> indica las props que el componente recibirá
  * ({ nextForm }) -> desestructuración de las props, accedemos directamente a nextForm
  */
-const PersonalForm: React.FC<PersonalFormProps> = ({ nextForm }) => { 
+const PersonalForm: React.FC<PersonalFormProps> = ({ nextForm, resetForm }) => { 
     // estado para guardar los datos del cuestionario del JSON
     const [cuestionario, setCuestionario] = useState<Cuestionario | null>(null);
     
     // estado para guardar las respuestas
-    const [respuestas, setRespuestas] = useState<{ [key: string]: string}>({});
+    const [respuestas, setRespuestas] = useState<{[key: string]: string}>({});
     
     // estado para guardar los errores de validación
-    const [errores, setErrores] = useState<string>('');
+    const [errores, setErrores] = useState<string | null>(null);
     
     // cargamos el cuestionario del JSON
     useEffect(() => {
         const fetchCuestionario = async () => {
             try {
-              const response = await fetch('/assets/cuestionarios.json');
-              const data: Cuestionario[] = await response.json();
-              setCuestionario(data[0]);
+                const response = await fetch('/assets/cuestionarios.json');
+                const data: Cuestionario[] = await response.json();
+                setCuestionario(data[0]);
+
+                // cargamos las respuestas
+                /*cuestionario!.preguntas.map((pregunta) => {
+                    // const arrayRespuestas = [];
+                    // arrayRespuestas.push({[pregunta.id]: pregunta.respuesta});
+                    setRespuestas((prevRespuestas) => ({
+                        ...prevRespuestas,
+                        [pregunta.id]: pregunta.respuesta,
+                    }));
+                })*/
+
+                const respuestasIniciales: { [key: string]: any } = {};
+                data[0].preguntas.forEach((pregunta) => {
+                    respuestasIniciales[pregunta.id] = pregunta.respuesta;
+                });
+
+                setRespuestas(respuestasIniciales);
+              
             } catch (error) {
               console.error('Error al cargar el cuestionario de datos personales:', error);
             }
@@ -59,30 +80,37 @@ const PersonalForm: React.FC<PersonalFormProps> = ({ nextForm }) => {
 
         fetchCuestionario();
     }, []); // se ejecutará solo al montar el componente
+
     
+    // validación de formulario y transición al siguiente
+    // cuando el estado de un form cambia (se completan los campos) pasar al siguiente
+    // Hook para guardar en localStorage las respuestas cada vez que cambian
     useEffect(() => {
-        console.log('Respuestas actualizadas:', respuestas);
+        localStorage.setItem('respuestasPersonal', JSON.stringify(respuestas));
     }, [respuestas]);
-    
+
 
     const handleRespuestaChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>, id: string) => {
         let value = e.target.value;
 
         // verficamos si el evento proviene de un input type checkbox
+        // hay que modificarlo!!!
         if(e.target instanceof HTMLInputElement && e.target.type === 'checkbox') {
             value = e.target.checked? 'on' : '';
         }
 
+        // Actualizamos las respuestas en el componente padre usando la función setRespuestas
         setRespuestas((prevRespuestas) => ({
-           ...prevRespuestas,
-           [id]: value,
-        }))
+            ...prevRespuestas,
+            [id]: value,
+        }));
+        console.log('respuestas handleRespuestaChange:', respuestas);
     }
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        console.log('Respuestas:', respuestas);
-        // guardar en localStorage?
+        
+        console.log('Respuestas handleSubmit:', respuestas);
 
         // validación del formulario
         const incompleteFields = Object.values(respuestas).some((respuesta) => respuesta === '');
@@ -92,7 +120,7 @@ const PersonalForm: React.FC<PersonalFormProps> = ({ nextForm }) => {
             return;
         }
 
-        setErrores(''); // limpieza de errores si están todos los campos completos y validados
+        setErrores(null); // limpieza de errores si están todos los campos completos y validados
         nextForm(); // pasamos al siguiente formulario
     }
 
@@ -150,7 +178,8 @@ const PersonalForm: React.FC<PersonalFormProps> = ({ nextForm }) => {
                     </div>
                 }
 
-                <div className="d-flex justify-content-center">
+                <div className="d-flex justify-content-between">
+                    <button type="button" className="btn btn-primary" onClick={() => resetForm()}>Reset</button>
                     <button type="submit" className="btn btn-primary">Siguiente</button>
                 </div>
                 
